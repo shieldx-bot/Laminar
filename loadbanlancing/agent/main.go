e640d6becd100a2ef7ce19ba2e282f9699270f9b
shieldx <anhnv.24810310060@epu.edu.vn>
2026-01-12 22:25:54 +0700

    feat: implement agent structure and calculation functions for load balancing

---
+++ loadbanlancing/agent/main.go
@@ -0,0 +1,68 @@
+package agent
+
+var (
+	SLA float64 = 0.5 // 0.3 seconds
+)
+
+type Agent struct {
+	Gj   float64
+	Lvmj float64
+	LB   int
+}
+
+func AgentMain(metrix map[string]interface{}) (Agent, error) {
+	Gj := CalculateGj(metrix)
+	_ = Gj
+	Lvmj := CalculateLvmj(metrix)
+	_ = Lvmj
+	LB := CalculateLB(metrix)
+	_ = LB
+
+	return Agent{
+		Gj:   Gj,
+		Lvmj: Lvmj,
+		LB:   LB,
+	}, nil
+}
+
+func CalculateETj(metrix map[string]interface{}) float64 {
+	var ETj float64
+	var TLi float64
+	TLi = float64(metrix["TLi"].(int64))
+	ETj = (TLi) / float64(metrix["Penumj"].(int64)) * float64(metrix["Pemips"].(int64))
+	return ETj
+}
+
+func CalculateGj(metrix map[string]interface{}) float64 {
+	var Gj float64
+	Gj = CalculateETj(metrix) + metrix["TTj"].(float64)
+
+	return Gj
+}
+
+func CalculateLvmj(metrix map[string]interface{}) float64 {
+	var Lvmj float64
+	Lvmj = (float64(metrix["TLi"].(int64)) / float64(metrix["Pemips"].(int64)))
+	return Lvmj
+
+}
+
+func CalculateLB(metrix map[string]interface{}) int {
+	var LB int
+	var TLD float64
+	var MaxCapity float64
+	MaxCapity = SLA * float64(metrix["Pemips"].(int64))
+	TLD = float64(CalculateTLD(metrix)) // milliseconds
+	if TLD < MaxCapity {
+		LB = 1
+	} else {
+		LB = 0
+	}
+	return LB
+}
+
+func CalculateTLD(metrix map[string]interface{}) float64 {
+	var TLD float64
+	TLD = float64(metrix["TimeDoneTask"].(int64)) * float64(metrix["Pemips"].(int64))
+	return TLD
+}
