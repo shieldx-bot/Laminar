cc7aad18aff9cef85ad1ca137a5363d9ff8782c8
shieldx <anhnv.24810310060@epu.edu.vn>
2026-01-14 19:01:33 +0700

    fix: remove database connection code from main.go

--- go-services/main.go
+++ go-services/main.go
@@ -1,13 +1,5 @@
 package main
 
-import "database/sql"
-
 func main() {
-	connStr := "host=34.177.91.6 port=5432 user=postgres password=Vananh12345@ dbname=laminar sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		panic(err)
-	}
-	db.QueryRow("
-	select *  from ")
+	// Intentionally empty. Executables are in ./cmd.
 }
