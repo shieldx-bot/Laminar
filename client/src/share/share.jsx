aa182c226fe67cae7b47a8d5a314d7818502f9be
shieldx <anhnv.24810310060@epu.edu.vn>
2026-01-18 11:59:19 +0700

    refactor: uncomment backend IPs in shareDataServer for active usage

--- client/src/share/share.jsx
+++ client/src/share/share.jsx
@@ -1,7 +1,7 @@
 export const shareDataServer = [
-    // { IP: "43.207.121.197:8082", latency: 0 }, // backend 8
-    // { IP: "54.95.111.110:8082", latency: 0 }, // backend 7
-    // { IP: "54.249.135.230:8082", latency: 0 }, // backend 6
+    { IP: "43.207.121.197:8082", latency: 0 }, // backend 8
+    { IP: "54.95.111.110:8082", latency: 0 }, // backend 7
+    { IP: "54.249.135.230:8082", latency: 0 }, // backend 6
     // { IP: "13.114.152.22:50054", latency: 0 }, // postgre SQL
     { IP: "35.247.171.94:8082", latency: 0 }, // backend 2
     { IP: "34.143.172.6:8082", latency: 0 }, // backend 1
